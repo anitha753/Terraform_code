d229e1cb5e40458b409d2f6867bb4495a970cd0d
anitha753 <122053347+anitha753@users.noreply.github.com>
2023-03-02 23:56:25 +0530

    Create MAP-name-labels-for-instance.tf

---
+++ MAP-name-labels-for-instance.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+region = "ap-south-1"
+access_key = "AKIAQIZETP44KJS3CLAX"
+secret_key = "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"
+}
+resource "aws_instance" "key" {
+ami=""
+instance_type = "t2.micro"
+tags = {
+Name = var.abc
+}
+}
+variable "abc" {
+type = map(string)
+default = {
+Project = "SWIGGY"
+Environment = "DEV"
+Name = "CCIT"
+}
+}
+  
